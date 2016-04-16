d3527aabf2ac3df958c6154f69affacc6c33d95f
Rentaro Kinoshita <rk7fd3s@gmail.com>
2016-04-17 08:14:45 +0900

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the weeApp
+ */
+angular.module('weeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weeApp
+ */
+angular.module('weeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
